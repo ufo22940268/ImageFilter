b185b85ff196d8e57abc11b76cc31ccfdc9b7a8b
ufo22940268 <ufo22940268@gmail.com>
2013-05-25 13:42:05 +0800

    asdf

---
+++ assets/js/my.js
@@ -0,0 +1,20 @@
+$('#open-popup').magnificPopup({
+    items: [
+      {
+        src: 'assets/img/1.jpg',
+      },
+      {
+        src: 'assets/img/2.jpg',
+      },
+      {
+        src: 'assets/img/3.jpg',
+      },
+      {
+        src: 'assets/img/4.jpg',
+      },
+    ],
+    gallery: {
+      enabled: true
+    },
+    type: 'image' // this is a default type
+});//@ sourceURL=pen.js</script>
